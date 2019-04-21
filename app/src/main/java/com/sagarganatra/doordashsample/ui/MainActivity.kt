a4095559cd852aead54a476ac14dc00bc17aac27
sagarkg <sagar.ganatra@gmail.com>
2019-04-20 20:19:38 -0400

    Added manifest

--- app/src/main/java/com/sagarganatra/doordashsample/ui/MainActivity.kt
+++ app/src/main/java/com/sagarganatra/doordashsample/ui/MainActivity.kt
@@ -1,14 +1,14 @@
-package com.sagarganatra.doordashsample
+package com.sagarganatra.doordashsample.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sagarganatra.doordashsample.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 }
