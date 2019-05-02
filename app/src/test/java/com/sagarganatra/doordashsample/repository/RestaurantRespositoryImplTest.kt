1a9b6c125e3e643820be10fb4898038f4bee114f
sagarkg <sagar.ganatra@gmail.com>
2019-05-01 21:44:45 -0400

    fixed typo

--- app/src/test/java/com/sagarganatra/doordashsample/repository/RestaurantRespositoryImplTest.kt
+++ app/src/test/java/com/sagarganatra/doordashsample/repository/RestaurantRespositoryImplTest.kt
@@ -1,23 +1,18 @@
 package com.sagarganatra.doordashsample.repository
 
-import com.google.gson.Gson
-import com.nhaarman.mockitokotlin2.mock
 import com.sagarganatra.doordashsample.api.RestaurantsApi
 import com.sagarganatra.doordashsample.models.Restaurant
-import com.sagarganatra.doordashsample.utils.BASE_URL
-import com.sagarganatra.doordashsample.utils.getRestaurantListSampleResponse
 import io.reactivex.observers.TestObserver
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.mockito.ArgumentMatchers
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RestaurantRespositoryImplTest {
+class RestaurantRepositoryImplTest {
 
     private val mockWebServer = MockWebServer()
 
